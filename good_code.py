157579eff82912b98a3d55db6384d0579f741b7f
JeremyArancio <jeremyarancio@gmail.com>
2024-03-06 15:20:03 +0100

    Good code

--- good_code.py
+++ good_code.py
@@ -1,106 +1,125 @@
 from abc import ABC, abstractmethod
 from typing import List
+import logging
 
 import pandas as pd
 import numpy as np
 from sklearn.base import TransformerMixin
+from sklearn.preprocessing import LabelEncoder
+
+logging.basicConfig(level=logging.INFO)
 
 
 class FeatureProcessor(ABC):
+    def __init__(self, feature_names: List[str]) -> None:
+        self.feature_names = feature_names
+
     @abstractmethod
-    def process(feature: pd.Series) -> pd.Series:
+    def process(self, df: pd.DataFrame) -> pd.DataFrame:
         pass
 
 
-class Normalizer(FeatureProcessor):
-    def process(feature: pd.Series) -> pd.Series:
-        std = np.std(feature)
-        mean = np.mean(feature)
-        return (feature - mean) / std
+class Normalizer(FeatureProcessor):   
+    def process(self, df: pd.DataFrame) -> pd.DataFrame:
+        features = df[self.feature_names]
+        std = np.std(features)
+        mean = np.mean(features)
+        return (features - mean) / std
 
 
 class Standardizer(FeatureProcessor):
-    def process(feature: pd.Series) -> pd.Series:
-        minimum = feature.min()
-        maximum = feature.max()
-        return (feature - minimum) / (maximum - minimum)
+    def process(self, df: pd.DataFrame) -> pd.DataFrame:
+        features = df[self.feature_names]
+        minimum = features.min()
+        maximum = features.max()
+        return (features - minimum) / (maximum - minimum)
 
 
 class Encoder(FeatureProcessor):
-    def __init__(self, encoder: TransformerMixin) -> None:
-        self.encoder = encoder
 
-    def process(self, feature: pd.Series) -> pd.Series:
-        array = self.encoder.fit_transform(feature)
-        return pd.Series(array, name=feature.name)
+    def __init__(self, encoder: TransformerMixin, feature_names: List[str]) -> None:
+        self.encoder = encoder
+        super().__init__(feature_names)
+
+    def process(self, df: pd.DataFrame) -> pd.DataFrame:
+        features = df[self.feature_names]
+        array = self.encoder.fit_transform(features)
+        array = np.atleast_2d(array) # Transform array into 2D from 1D or 2D arrays
+        processed_df = pd.DataFrame({name: data for name, data in zip(features.columns, array)})
+        return processed_df
     
 
 class NaFiller(FeatureProcessor):
-    def process(self, feature: pd.Series, value: int = -1) -> pd.Series:
-        return feature.fillna(value=value)
+
+    def __init__(self, feature_names: List[str], value: int = -1) -> None:
+        self.value = value
+        super().__init__(feature_names)
+
+    def process(self, df: pd.DataFrame) -> pd.DataFrame:
+        features = df[self.feature_names]
+        return features.fillna(value=self.value)
 
 
 class DataLoader(ABC):
-    @abstractmethod
-    def load_data(path: str) -> pd.DataFrame:
+    @abstractmethod   
+    def load_data(self, path: str) -> pd.DataFrame:
         pass
 
 
 class ParquetDataLoader(DataLoader):
-    def load_data(path: str) -> pd.DataFrame:
+    def load_data(self, path: str) -> pd.DataFrame:
         return pd.read_parquet(path)
     
 
 class DataSaver(ABC):
     @abstractmethod
-    def save(df: pd.DataFrame, path: str) -> None:
+    def save_data(self, df: pd.DataFrame, path: str) -> None:
         pass
 
 
 class ParquetDataSaver(DataSaver):
-    def save(df: pd.DataFrame, path: str) -> None:
+    def save_data(self, df: pd.DataFrame, path: str) -> None:
         df.to_parquet(path)
 
 
 class DataProcessor(ABC):
     @abstractmethod
-    def process(path: str, output_path: str) -> None:
+    def process(self, path: str, output_path: str) -> None:
         pass
 
 
 class ExampleDataProcessor(DataProcessor):
-
     def __init__(
         self,
-        numerical_processor: FeatureProcessor,
-        categorical_processor: FeatureProcessor,
-        na_filler: NaFiller,
-        data_loader: DataLoader, #TODO
-        data_saver: DataSaver #TODO
+        feature_processors: List[FeatureProcessor],
+        data_loader: DataLoader,
+        data_saver: DataSaver
     ) -> None:
-        self.numerical_processor = numerical_processor
-        self.categorical_processor = categorical_processor
-        self.na_filler = na_filler
+        self.feature_processors = feature_processors
         self.data_loader = data_loader
         self.data_saver = data_saver     
 
     def process(self, path: str, output_path: str) -> None:
-        df = self.data_loader.load_data(path) # TODO
-        normalized_feature = self.numerical_processor.process(df["feature_a"])
-        encoded_feature = self.categorical_processor.process(df["feature_b"])
-        filled_feature = self.na_filler.process(df["feature_c"])
-        processed_df = compose_df(
-            normalized_feature, 
-            encoded_feature, 
-            filled_feature,
-            column_names=df.columns
+        df = self.data_loader.load_data(path)
+        processed_df = pd.concat(
+            [feature_processor.process(df) for feature_processor in self.feature_processors],
+            axis=1
         )
-        self.data_saver.save(df=processed_df, path=output_path) #TODO
+        self.data_saver.save_data(df=processed_df, path=output_path)
+        logging.info(f"Processed df: {processed_df}")
 
-
-def compose_df(*args, column_names: List[str]) -> pd.DataFrame:
-    data = {column_name: series for column_name, series in zip(column_names, args) }
-    return pd.DataFrame(data)
 
 if __name__ == "__main__":
-    processor
+    processor = ExampleDataProcessor(
+        feature_processors=[
+            Normalizer(feature_names=["feature_a"]),
+            Encoder(encoder=LabelEncoder(), feature_names=["feature_b"]),
+            NaFiller(feature_names=["feature_c"], value=5)
+        ],
+        data_loader=ParquetDataLoader(),
+        data_saver=ParquetDataSaver()
+    )
+    processor.process(
+        path="data/data.parquet", 
+        output_path="data/preprocessed_data.parquet"
+    )
